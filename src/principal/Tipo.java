872ba823669846a099f0d2fc25ececae7dcc4fb5
Jav4532 <batlejavier62001@gmail.com>
2023-03-27 12:20:34 +0200

    Base con error en la clase Tipo

--- src/principal/Tipo.java
+++ src/principal/Tipo.java
@@ -1,11 +1,44 @@
 package principal;
 
-public class Tipo {
+public enum Tipo {
+    AGUA,
+    FUEGO,
+    PLANTA,
+    BICHO,
+    VOLADOR,
+    ELECTRICO,
+    TIERRA,
+    DRAGON,
+    VENENO,
+    ROCA,
+    PSQUICO,
+    FANTASMA;   
 
-	public Tipo(String string) {
-		// TODO Auto-generated constructor stub
-	}
+    public double multiplicador(Tipo otro) {
+        switch (this) {
+            case AGUA:
+                switch (otro) {
+                    case FUEGO:
+                    case TIERRA:
+                    case ROCA:
+                        return 2.0;
+                    case AGUA:
+                    case PLANTA:
+                    case DRAGON:
+                        return 0.5;
+                    default:
+                        return 1.0;
+                }
 
-	
-
-}
+            case FUEGO:
+                switch (otro) {
+                    case AGUA:
+                    case TIERRA:
+                    case ROCA:
+                        return 0.5;
+                    case PLANTA:
+                    case BICHO:
+                        return 2.0;
+                    default:
+                        return 1.0;
+                }

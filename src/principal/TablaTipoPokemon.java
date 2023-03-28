15118b7ea9bc7d9193bccc0e2aa308a8f1170c4b
Johansr4 <juan07ruiz07@gmail.com>
2023-03-28 09:38:23 +0200

    Implementacion de clase Eficacias Y Tipo pokemon

--- src/principal/TablaTipoPokemon.java
+++ src/principal/TablaTipoPokemon.java
@@ -34,5 +34,140 @@ public class TablaTipoPokemon {
             {0, 1, 1, 1, 1, 0.5, 1, 2, 1, 1, 1, 2, 1, 0.5, 1, 0.5, 0.5, 1}, // VENENO
             {0.5, 1, 2, 1, 0.5, 1, 1, 1, 1, 2, 1, 2, 1, 0.5, 1, 1, 1, 1} // VOLADOR
         };
+
+        this.efectividadesPokemon = efectividadesPokemon;
     }
+
+    private TipoPokemon getTipoPokemonAt(int index) {
+        if (index >= 0 && index < TipoPokemon.values().length) {
+            return TipoPokemon.values()[index];
+        }
+        return null;
+    }
+
+    /**
+     * Debilidades del tipo pasado
+     *
+     * @param tipo
+     * @return
+     */
+    public ArrayList<TipoPokemon> debilidades(TipoPokemon tipo) {
+
+        ArrayList<TipoPokemon> debilidades = new ArrayList<>();
+
+        for (int i = 0; i < efectividadesPokemon.length; i++) {
+
+            if (efectividadesPokemon[i][tipo.getIndice()] == EficaciasPokemon.ES_MUY_EFICAZ.getEficacia()) {
+                debilidades.add(this.getTipoPokemonAt(i));
+            }
+
+        }
+
+        return debilidades;
+
+    }
+
+    /**
+     * Debilidades comunes de los tipos pasados
+     *
+     * @param tipo1
+     * @param tipo2
+     */
+    public ArrayList<TipoPokemon> debilidades(TipoPokemon tipo1, TipoPokemon tipo2) {
+
+        double efectividadFinal = 0;
+        ArrayList<TipoPokemon> debilidades = new ArrayList<>();
+
+        for (int i = 0; i < efectividadesPokemon.length; i++) {
+
+            efectividadFinal = efectividadesPokemon[i][tipo1.getIndice()]
+                    * efectividadesPokemon[i][tipo2.getIndice()];
+
+            if (efectividadFinal >= EficaciasPokemon.ES_MUY_EFICAZ.getEficacia()) {
+                debilidades.add(this.getTipoPokemonAt(i));
+            }
+        }
+
+        return debilidades;
+
+    }
+
+    /**
+     * Eficacias del tipo pasado
+     *
+     * @param tipo
+     */
+    public ArrayList<TipoPokemon> eficacias(TipoPokemon tipo) {
+
+        ArrayList<TipoPokemon> eficacias = new ArrayList<>();
+        
+        for (int j = 0; j < efectividadesPokemon.length; j++) {
+
+            if (efectividadesPokemon[tipo.getIndice()][j] == EficaciasPokemon.ES_MUY_EFICAZ.getEficacia()) {
+                eficacias.add(this.getTipoPokemonAt(j));
+            }
+
+        }
+
+        return eficacias;
+        
+    }
+
+    /**
+     * Informacion del tipo pasado
+     *
+     * @param tipo
+     */
+    public String infoTipo(TipoPokemon tipo) {
+
+        String resultado = "";
+        for (int j = 0; j < efectividadesPokemon.length; j++) {
+            resultado += this.getTipoPokemonAt(j) + " " + mostrarEficacia(efectividadesPokemon[tipo.getIndice()][j]);
+        }
+        return resultado;
+
+    }
+
+    /**
+     * Mensaje Eficacia
+     *
+     * @param eficacia
+     * @return
+     */
+    private String mostrarEficacia(double eficacia) {
+
+        for (EficaciasPokemon eficaciaPokemon: EficaciasPokemon.values()) {
+            if(eficaciaPokemon.getEficacia() == eficacia){
+                return eficaciaPokemon.getMensaje();
+            }
+        }
+
+        return null;
+
+    }
+
+    /**
+     * Muestra la eficacia de un tipo a otro tipo
+     *
+     * @param tipoAtacante
+     * @param tipoOponente
+     */
+    public String mostrarEficaciaTipo(TipoPokemon tipoAtacante, TipoPokemon tipoOponente) {
+        return mostrarEficacia(efectividadesPokemon[tipoAtacante.getIndice()][tipoOponente.getIndice()]);
+    }
+
+    /**
+     * Muestra la eficacia de un tipo a un pokemon con dos tipos
+     *
+     * @param tipoAtacante
+     * @param tipo1Oponente
+     * @param tipo2Oponente
+     */
+    public String mostrarEficaciaTipo(TipoPokemon tipoAtacante, TipoPokemon tipo1Oponente, TipoPokemon tipo2Oponente) {
+        //multiplico las efectivadades
+        double efectividad = efectividadesPokemon[tipoAtacante.getIndice()][tipo1Oponente.getIndice()] 
+                * efectividadesPokemon[tipoAtacante.getIndice()][tipo2Oponente.getIndice()];
+        return mostrarEficacia(efectividad);
+    }
+
 }

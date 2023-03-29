5575f47e364bd67c0d3bf18af0fe41f9d5ad9508
Jav4532 <batlejavier62001@gmail.com>
2023-03-29 11:34:05 +0200

    Borrador Clase Captura

--- src/principal/Captura.java
+++ src/principal/Captura.java
@@ -1,10 +1,73 @@
 package principal;
 
-public class Captura {
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+public class Captura extends JFrame {
+
+	private ArrayList<String> palabrasProhibidas;
+	private Pokemon nuevoPokemon;
+	private ArrayList<Pokemon> cajaPokemon;
+	private JPanel panel;
+	private JLabel imagenPokemon;
+	private JButton botonRandom;
+	private JButton botonCapturar;
+	private JTextField campoMote;
+
+	public Captura() {
+		super("Captura de Pokemon");
+
+		palabrasProhibidas = new ArrayList<String>();
+		palabrasProhibidas.add("Pene");
+		// Agrega aquí todas las palabras prohibidas
+
+		cajaPokemon = new ArrayList<Pokemon>();
+
+		panel = new JPanel();
+		imagenPokemon = new JLabel();
+		botonRandom = new JButton("Random");
+		botonRandom.addActionListener(e -> generarPokemonAleatorio());
+		botonCapturar = new JButton("Capturar");
+		botonCapturar.addActionListener(e -> capturarPokemon());
+		campoMote = new JTextField(15);
+
+		panel.add(imagenPokemon);
+		panel.add(botonRandom);
+		panel.add(botonCapturar);
+		panel.add(campoMote);
+
+		add(panel);
+
+		setSize(400, 300);
+		setVisible(true);
 	}
 
-}
+	private void generarPokemonAleatorio() {
+		Random random = new Random();
+		String[] nombresPokemon = { "Pikachu" };
+		String nombre = nombresPokemon[random.nextInt(nombresPokemon.length)];
+		int nivel = 1;
+		nuevoPokemon = new Pokemon(nombre, nivel);
+		actualizarImagenPokemon();
+	}
+
+	private void actualizarImagenPokemon() {
+		imagenPokemon.setText(nuevoPokemon.getNombre());
+	}
+
+	private void capturarPokemon() {
+		if (nuevoPokemon == null) {
+			return;
+		}
+		
+	}
+		
+	
+

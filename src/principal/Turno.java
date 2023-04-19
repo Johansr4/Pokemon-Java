5cdc8db7a93cb88fa227baa6d54e513e58bea856
Jav4532 <batlejavier62001@gmail.com>
2023-04-19 09:33:11 +0200

    HUD

--- src/principal/Turno.java
+++ src/principal/Turno.java
@@ -1,32 +1,34 @@
 package principal;
 
 public class Turno {
-    private int numeroTurno;
-    private String accionEntrenador;
-    private String accionRival;
-    
-    public Turno(int numeroTurno, String accionEntrenador, String accionRival) {
-        this.numeroTurno = numeroTurno;
-        this.accionEntrenador = accionEntrenador;
-        this.accionRival = accionRival;
-    }
-    
-    public int getNumeroTurno() {
-        return numeroTurno;
-    }
-    
-    public String getAccionEntrenador() {
-        return accionEntrenador;
-    }
-    
-    public String getAccionRival() {
-        return accionRival;
-    }
-    
-    @Override
-    public String toString() {
-        return "Turno " + numeroTurno + ":" + "\n" +
-               "Entrenador: " + accionEntrenador + "\n" +
-               "Rival: " + accionRival;
-    }
+	private int numeroTurno;
+	private String accionEntrenador;
+	private String accionRival;
+
+	public Turno(int numeroTurno, String accionEntrenador, String accionRival) {
+		this.numeroTurno = numeroTurno;
+		this.accionEntrenador = accionEntrenador;
+		this.accionRival = accionRival;
+	}
+
+	public int getNumeroTurno() {
+		return numeroTurno;
+	}
+
+	public String getAccionEntrenador() {
+		return accionEntrenador;
+	}
+
+	public String getAccionRival() {
+		return accionRival;
+	}
+
+	@Override
+	public String toString() {
+		return "Turno " + numeroTurno + ":" + "\n" + "Entrenador: " + accionEntrenador + "\n" + "Rival: " + accionRival;
+	}
+
+	public enum Accion {
+		ATACAR, CAMBIAR, USAR_OBJETO, RENDIRSE
+	}
 }

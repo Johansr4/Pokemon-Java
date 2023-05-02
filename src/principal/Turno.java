dcafc4867aeb35df3a2a3b830769cfaa5e78438e
Jav4532 <batlejavier62001@gmail.com>
2023-05-02 22:24:54 +0200

    Turno update

--- src/principal/Turno.java
+++ src/principal/Turno.java
@@ -3,34 +3,47 @@ package principal;
 //Texto de lo ocurrido en dicho turno
 
 public class Turno {
-	private int numeroTurno;
-	private String accionEntrenador;
-	private String accionRival;
+    private int numeroTurno;
+    private String accionEntrenador;
+    private String accionRival;
+    private Pokemon pokemon;
 
-	public Turno(int numeroTurno, String accionEntrenador, String accionRival) {
-		this.numeroTurno = numeroTurno;
-		this.accionEntrenador = accionEntrenador;
-		this.accionRival = accionRival;
-	}
+    public Turno(int numeroTurno, String accionEntrenador, String accionRival, Pokemon pokemon) {
+        this.numeroTurno = numeroTurno;
+        this.accionEntrenador = accionEntrenador;
+        this.accionRival = accionRival;
+        this.pokemon = pokemon;
+    }
 
-	public int getNumeroTurno() {
-		return numeroTurno;
-	}
+    public int getNumeroTurno() {
+        return numeroTurno;
+    }
 
-	public String getAccionEntrenador() {
-		return accionEntrenador;
-	}
+    public String getAccionEntrenador() {
+        return accionEntrenador;
+    }
 
-	public String getAccionRival() {
-		return accionRival;
-	}
+    public String getAccionRival() {
+        return accionRival;
+    }
 
-	@Override
-	public String toString() {
-		return "Turno " + numeroTurno + ":" + "\n" + "Entrenador: " + accionEntrenador + "\n" + "Rival: " + accionRival;
-	}
+    public Pokemon getPokemon() {
+        return pokemon;
+    }
 
-	public enum Accion {
-		ATACAR, CAMBIAR, USAR_OBJETO, RENDIRSE
-	}
+    public String resumenTurno() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Turno ").append(numeroTurno).append(": ");
+        if (pokemon != null) {
+            sb.append(pokemon.getNombre()).append(" ");
+        }
+        sb.append("Entrenador: ").append(accionEntrenador).append(", ");
+        sb.append("Rival: ").append(accionRival);
+        return sb.toString();
+    }
+
+    public enum Accion {
+        ATACAR, CAMBIAR, USAR_OBJETO, RENDIRSE
+    }
 }
+

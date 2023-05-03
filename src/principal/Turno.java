e1af60e349901029f4932d44d6f3598e78cef0de
Jav4532 <batlejavier62001@gmail.com>
2023-05-03 10:02:20 +0200

    Mas metodos en Turno

--- src/principal/Turno.java
+++ src/principal/Turno.java
@@ -3,47 +3,55 @@ package principal;
 //Texto de lo ocurrido en dicho turno
 
 public class Turno {
-    private int numeroTurno;
-    private String accionEntrenador;
-    private String accionRival;
-    private Pokemon pokemon;
+	private int numeroTurno;
+	private String accionEntrenador;
+	private String accionRival;
+	private Pokemon pokemon;
 
-    public Turno(int numeroTurno, String accionEntrenador, String accionRival, Pokemon pokemon) {
-        this.numeroTurno = numeroTurno;
-        this.accionEntrenador = accionEntrenador;
-        this.accionRival = accionRival;
-        this.pokemon = pokemon;
-    }
+	public Turno(int numeroTurno, String accionEntrenador, Pokemon pokemon) {
+		this.numeroTurno = numeroTurno;
+		this.accionEntrenador = accionEntrenador;
+		this.pokemon = pokemon;
+	}
 
-    public int getNumeroTurno() {
-        return numeroTurno;
-    }
+	public int getNumeroTurno() {
+		return numeroTurno;
+	}
 
-    public String getAccionEntrenador() {
-        return accionEntrenador;
-    }
+	public String getAccionEntrenador() {
+		return accionEntrenador;
+	}
 
-    public String getAccionRival() {
-        return accionRival;
-    }
+	public String getAccionRival() {
+		return accionRival;
+	}
 
-    public Pokemon getPokemon() {
-        return pokemon;
-    }
+	public Pokemon getPokemon() {
+		return pokemon;
+	}
 
-    public String resumenTurno() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Turno ").append(numeroTurno).append(": ");
-        if (pokemon != null) {
-            sb.append(pokemon.getNombre()).append(" ");
-        }
-        sb.append("Entrenador: ").append(accionEntrenador).append(", ");
-        sb.append("Rival: ").append(accionRival);
-        return sb.toString();
-    }
+	public void setAccionEntrenador(String accionEntrenador) {
+		this.accionEntrenador = accionEntrenador;
+	}
 
-    public enum Accion {
-        ATACAR, CAMBIAR, USAR_OBJETO, RENDIRSE
-    }
+	public void setAccionRival(String accionRival) {
+		this.accionRival = accionRival;
+	}
+
+	public String resumenTurno() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Turno ").append(numeroTurno).append(": ");
+		sb.append("Entrenador: ").append(accionEntrenador);
+		if (accionRival != null) {
+			sb.append(". Rival: ").append(accionRival);
+		}
+		if (pokemon != null) {
+			sb.append(" ").append(pokemon.getNombre()).append(" ");
+		}
+		return sb.toString();
+	}
+
+	public enum Accion {
+		ATACAR, CAMBIAR, USAR_OBJETO, RENDIRSE
+	}
 }
-

0b7add44bf1fa68a4b46b8a6436a230766083ba5
Johansr4 <juan07ruiz07@gmail.com>
2023-05-09 16:24:19 +0200

    Base vista Captura y CrearUsuario

--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -1,28 +1,36 @@
-	package controlador;
-	
+package controlador;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/vista/CapturaPokemon.fxml"));
+			// Parent root =
+			// FXMLLoader.load(getClass().getResource("/vista/CrearUsuario.fxml"));
 			
-			Parent root = FXMLLoader.load(getClass().getResource("/vista/CrearUsuario.fxml"));
+			
 			Scene scene = new Scene(root);
-			primaryStage.setTitle("PRUEBA FX JOHAN");
+			primaryStage.setTitle("Captura Pokemon");
+			// Agregar ícono
+
+			primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/icono/iconPk.png")));
+
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
--- src/modelo/Captura.java
+++ src/modelo/Captura.java
@@ -1,114 +1,118 @@
 package modelo;
 
-
 import java.util.Random;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-
 public class Captura {
-    private static final String[] PALABRAS_PROHIBIDAS = { "palabra1", "palabra2", "palabra3" }; // lista de palabras prohibidas
-    private static final Pattern PATRON_MOTE = Pattern.compile("^[a-zA-Z]+$"); // expresión regular para el mote
+	private static final String[] PALABRAS_PROHIBIDAS = { "palabra1", "palabra2", "palabra3" }; // lista de palabras
+																								// prohibidas
+	private static final Pattern PATRON_MOTE = Pattern.compile("^[a-zA-Z]+$"); // expresión regular para el mote
 
-    private static Pokemon nuevoPokemon;
-    private static int pokeballs = 5;
-    private static Pokemon[] caja = new Pokemon[10];
-    private static int indiceCaja = 0;
-    private static Scanner scanner = new Scanner(System.in);
-    private static Random random = new Random();
+	private static Pokemon nuevoPokemon;
+	private static int pokeballs = 5;
+	private static Pokemon[] caja = new Pokemon[10];
+	private static int indiceCaja = 0;
+	private static Scanner scanner = new Scanner(System.in);
+	private static Random random = new Random();
 
-    public Captura() {
-        nuevoPokemon = new Pokemon();
-    }
+	public Captura() {
+		nuevoPokemon = new Pokemon();
+	}
 
-    public void capturarPokemonWhile() {
-        boolean capturado = false;
-        while (!capturado) {
-            nuevoPokemon = new Pokemon();
-            mostrarPokemon();
-            capturado = capturarPokemon();
-        }
-        agregarACaja();
-    }
-    
+	public void capturarPokemonWhile() {
+		boolean capturado = false;
+		while (!capturado) {
+			nuevoPokemon = new Pokemon();
+			mostrarPokemon();
+			capturado = capturarPokemon();
+		}
+		agregarACaja();
+	}
 
-    public void mostrarPokemon() {
-        System.out.println("Un " + nuevoPokemon.getNombre() + " salvaje aparecio!");
-        System.out.println("NIVEL: " + nuevoPokemon.getNivel());
-    }
+	public boolean capturarPokemon() {
+		pokeballs--;
+		boolean capturado = random.nextInt(3) < 2;
+		if (capturado) {
+			System.out.println(nuevoPokemon.getNombre() + " capturado!");
+			agregarACaja();
+		} else {
+			System.out.println(nuevoPokemon.getNombre() + " escapó!");
+		}
+		return capturado;
+	}
 
-    public boolean capturarPokemon() {
-        pokeballs--;
-        boolean capturado = random.nextInt(3) < 2;
-        if (capturado) {
-            System.out.println(nuevoPokemon.getNombre() + " capturado!");
-        } else {
-            System.out.println(nuevoPokemon.getNombre() + " escapó!");
-        }
-        return capturado;
-    }
+	public boolean agregarACaja() {
+		/*System.out.println("¿Quieres ponerle un mote al nuevo Pokemon? (s/n)");
+		String opcion = scanner.next().toLowerCase();
+		if (opcion.equals("s")) {
+			boolean moteValido = false;
+			String mote = "";
+			while (!moteValido) {
+				System.out.println("Ingresa el mote del Pokemon (sólo letras):");
+				mote = scanner.next();
+				moteValido = PATRON_MOTE.matcher(mote).matches();
+				if (!moteValido) {
+					System.out.println("Mote inválido. El mote debe contener sólo letras.");
+				}
+			}
+			nuevoPokemon.setMote(mote);
+		}*/
+		if (indiceCaja < caja.length) {
+			caja[indiceCaja] = nuevoPokemon;
+			indiceCaja++;
+			System.out.println("El Pokemon ha sido añadido a la caja.");
+			return true;
+		} else {
+			System.out.println("La caja está llena. No se pudo añadir el Pokemon.");
+			return false;
+		}
+	}
 
-    public void agregarACaja() {
-        System.out.println("¿Quieres ponerle un mote al nuevo Pokemon? (s/n)");
-        String opcion = scanner.next().toLowerCase();
-        if (opcion.equals("s")) {
-            boolean moteValido = false;
-            String mote = "";
-            while (!moteValido) {
-                System.out.println("Ingresa el mote del Pokemon (sólo letras):");
-                mote = scanner.next();
-                moteValido = PATRON_MOTE.matcher(mote).matches();
-                if (!moteValido) {
-                    System.out.println("Mote inválido. El mote debe contener sólo letras.");
-                }
-            }
-            nuevoPokemon.setMote(mote);
-        }
-        if (indiceCaja < caja.length) {
-            caja[indiceCaja] = nuevoPokemon;
-            indiceCaja++;
-            System.out.println("El Pokemon ha sido añadido a la caja.");
-        } else {
-            System.out.println("La caja está llena. No se pudo añadir el Pokemon.");
-        }
-    }
+	public void mostrarCaja() {
+		for (int i = 0; i < indiceCaja; i++) {
+			Pokemon pokemon = caja[i];
+			String mote = pokemon.getMote();
+			String nombre = pokemon.getNombre();
+			String mensaje = i + 1 + ". " + nombre;
+			if (mote != null) {
+				mensaje += " (" + mote + ")";
+			}
+			System.out.println(mensaje);
+		}
+	}
 
-    public void mostrarCaja() {
-        for (int i = 0; i < indiceCaja; i++) {
-            Pokemon pokemon = caja[i];
-            String mote = pokemon.getMote();
-            String nombre = pokemon.getNombre();
-            String mensaje = i + 1 + ". " + nombre;
-            if (mote != null) {
-                mensaje += " (" + mote + ")";
-            }
-            System.out.println(mensaje);
-        }
-    }
+	public void liberarPokemon() {
+		mostrarCaja();
+		System.out.println("Ingresa el número del Pokemon que quieres liberar:");
+		int opcion = scanner.nextInt();
+		if (opcion > 0 && opcion <= indiceCaja) {
+			Pokemon pokemon = caja[opcion - 1];
+			System.out.println("¿Estás seguro de que quieres liberar a " + pokemon.getNombre() + "? (s/n)");
+			String confirmacion = scanner.next().toLowerCase();
+			if (confirmacion.equals("s")) {
+				for (int i = opcion - 1; i < indiceCaja - 1; i++) {
+					caja[i] = caja[i + 1];
+				}
+				indiceCaja--;
+				System.out.println(pokemon.getNombre() + " ha sido liberado.");
+			} else {
+				System.out.println(pokemon.getNombre() + " no ha sido liberado.");
+			}
+		} else {
+			System.out.println("Opción inválida.");
+		}
+	}
 
-    public void liberarPokemon() {
-        mostrarCaja();
-        System.out.println("Ingresa el número del Pokemon que quieres liberar:");
-        int opcion = scanner.nextInt();
-        if (opcion > 0 && opcion <= indiceCaja) {
-            Pokemon pokemon = caja[opcion - 1];
-            System.out.println("¿Estás seguro de que quieres liberar a " + pokemon.getNombre() + "? (s/n)");
-            String confirmacion = scanner.next().toLowerCase();
-            if (confirmacion.equals("s")) {
-                for (int i = opcion - 1; i < indiceCaja - 1; i++) {
-                    caja[i] = caja[i + 1];
-                }
-                indiceCaja--;
-                System.out.println(pokemon.getNombre() + " ha sido liberado.");
-            } else {
-                System.out.println(pokemon.getNombre() + " no ha sido liberado.");
-            }
-        } else {
-            System.out.println("Opción inválida.");
-        }
-    }
-    public Pokemon getNuevoPokemon() {
-        return this.nuevoPokemon;
-    }
+	public Pokemon getNuevoPokemon() {
+		return this.nuevoPokemon;
+	}
+
+	public String mostrarPokemon() {
+
+		String pokemonlbl = "Un " + nuevoPokemon.getNombre() + " de nivel " + nuevoPokemon.getNivel();
+				
+		return pokemonlbl;
+	}
+
 }
-
